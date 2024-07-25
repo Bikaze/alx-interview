bc5b7f56cdbf0a16473207364ed5a76477691a9a
Bikaze <clmntmugisha@gmail.com>
2024-07-25 12:57:04 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,5 +7,5 @@ def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding
     for one byte characters"""
     # check = [True if i < 256 else False for i in data]
-    check = [(i >> 7) == 0 for i in data]
+    check = [(i >> 7) <= 1 for i in data]
     return all(check)
