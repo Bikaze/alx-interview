ab93a197b2d1a0343a91b75912c4a26d2821b6c8
Bikaze <clmntmugisha@gmail.com>
2024-07-25 12:27:59 +0200

    Added documentation to the file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""""This module contains a function that determines if a given data set
+represents a valid UTF-8 encoding for one byte characters"""
 
 
 def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding
+    for one byte characters"""
     check = [True if i < 256 else False for i in data]
     return all(check)
