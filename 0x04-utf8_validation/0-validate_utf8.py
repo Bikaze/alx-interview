d04dc13cd4ec2e700d86bb09d806cd6da12bc2bc
Bikaze <clmntmugisha@gmail.com>
2024-07-25 14:59:18 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,43 @@ represents a valid UTF-8 encoding for one byte characters"""
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding
-    for one byte characters"""
-    # check = [True if i < 256 else False for i in data]
-    check = [(i >> 7) <= 1 for i in data]
-    return all(check)
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+
+    def is_valid_byte(byte):
+        return 0 <= byte <= 255
+
+    n = len(data)
+    i = 0
+
+    while i < n:
+        byte = data[i]
+
+        if byte > 255:
+            return False
+
+        if (byte >> 7) == 0:
+            # 1-byte character (ASCII), 0xxxxxxx
+            i += 1
+            continue
+        elif (byte >> 5) == 0b110:
+            # 2-byte character, 110xxxxx 10xxxxxx
+            num_bytes = 2
+        elif (byte >> 4) == 0b1110:
+            # 3-byte character, 1110xxxx 10xxxxxx 10xxxxxx
+            num_bytes = 3
+        elif (byte >> 3) == 0b11110:
+            # 4-byte character, 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+            num_bytes = 4
+        else:
+            return False
+
+        if i + num_bytes > n:
+            return False
+
+        for j in range(1, num_bytes):
+            if (data[i + j] >> 6) != 0b10:
+                return False
+
+        i += num_bytes
+
+    return True
