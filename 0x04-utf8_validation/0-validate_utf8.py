939ff88439ab41be101b6e2fc9ac1f19c134a2d9
Bikaze <clmntmugisha@gmail.com>
2024-07-26 10:58:28 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,38 +6,27 @@ represents a valid UTF-8 encoding for one byte characters"""
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding."""
 
-    n = len(data)
-    i = 0
+    num_bytes = 0
 
-    while i < n:
-        byte = data[i]
+    for byte in data:
+        # Get the 8 least significant bits of the byte
+        byte = byte & 0xFF
 
-        if byte > 255:
-            return False
-
-        if (byte >> 7) == 0:
-            # 1-byte character (ASCII), 0xxxxxxx
-            i += 1
-            continue
-        elif (byte >> 5) == 0b110:
-            # 2-byte character, 110xxxxx 10xxxxxx
-            num_bytes = 2
-        elif (byte >> 4) == 0b1110:
-            # 3-byte character, 1110xxxx 10xxxxxx 10xxxxxx
-            num_bytes = 3
-        elif (byte >> 3) == 0b11110:
-            # 4-byte character, 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-            num_bytes = 4
-        else:
-            return False
-
-        if i + num_bytes > n:
-            return False
-
-        for j in range(1, num_bytes):
-            if i + j >= n or (data[i + j] >> 6) != 0b10:
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
                 return False
+        else:
+            # Check if the byte is a valid continuation byte
+            if (byte >> 6) != 0b10:
+                return False
+            else:
+                num_bytes -= 1
 
-        i += num_bytes
-
-    return True
+    return num_bytes == 0
