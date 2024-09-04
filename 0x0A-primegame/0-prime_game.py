ce4290d053ecc63b4a581fcf8d8425f7da07ab57
Bikaze <clmntmugisha@gmail.com>
2024-09-04 19:18:51 +0200

    Solving linting issues

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Prime Game"""
+
 
 def isWinner(x, nums):
+    """Determine the winner of the game"""
     if x == 0 or not nums:
         return None
-    
+
     def sieve_of_eratosthenes(max_n):
+        """Sieve of Eratosthenes algorithm"""
         sieve = [True] * (max_n + 1)
         sieve[0] = sieve[1] = False
         for start in range(2, int(max_n**0.5) + 1):
@@ -15,7 +19,7 @@ def isWinner(x, nums):
 
     max_n = max(nums)
     prime_sieve = sieve_of_eratosthenes(max_n)
-    
+
     def count_prime_picks(n):
         # Count how many primes are there up to `n`
         prime_count = 0
@@ -23,17 +27,17 @@ def isWinner(x, nums):
             if prime_sieve[i]:
                 prime_count += 1
         return prime_count
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         prime_picks = count_prime_picks(n)
         if prime_picks % 2 == 0:
             ben_wins += 1  # Ben wins when prime_picks are even
         else:
             maria_wins += 1  # Maria wins when prime_picks are odd
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
