caa328d4ea26209e4f407e4e7d615d1fec820d88
Bikaze <clmntmugisha@gmail.com>
2024-09-04 19:22:43 +0200

    Attempted the challenge #4

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,40 +3,36 @@
 
 
 def isWinner(x, nums):
-    """Determine the winner of the game"""
-    if x == 0 or not nums:
+    """Determine the winner of the game."""
+    if x <= 0 or not nums:
         return None
 
-    def sieve_of_eratosthenes(max_n):
-        """Sieve of Eratosthenes algorithm"""
-        sieve = [True] * (max_n + 1)
-        sieve[0] = sieve[1] = False
-        for start in range(2, int(max_n**0.5) + 1):
-            if sieve[start]:
-                for i in range(start * start, max_n + 1, start):
-                    sieve[i] = False
-        return sieve
-
     max_n = max(nums)
-    prime_sieve = sieve_of_eratosthenes(max_n)
 
-    def count_prime_picks(n):
-        # Count how many primes are there up to `n`
-        prime_count = 0
-        for i in range(2, n + 1):
-            if prime_sieve[i]:
-                prime_count += 1
-        return prime_count
+    # Sieve of Eratosthenes algorithm to find all primes up to max_n
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
+
+    for start in range(2, int(max_n**0.5) + 1):
+        if sieve[start]:
+            for i in range(start * start, max_n + 1, start):
+                sieve[i] = False
+
+    # Precompute the number of primes up to each index
+    prime_count = [0] * (max_n + 1)
+    for i in range(2, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        prime_picks = count_prime_picks(n)
-        if prime_picks % 2 == 0:
-            ben_wins += 1  # Ben wins when prime_picks are even
+        if n == 1:
+            ben_wins += 1  # No primes, Ben wins automatically
+        elif prime_count[n] % 2 == 0:
+            ben_wins += 1  # Ben wins if prime count is even
         else:
-            maria_wins += 1  # Maria wins when prime_picks are odd
+            maria_wins += 1  # Maria wins if prime count is odd
 
     if maria_wins > ben_wins:
         return "Maria"
